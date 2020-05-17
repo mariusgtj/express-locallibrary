a6bc57da988ec7d70db6daf6ce5c5073d268a4e4
mariusgtj <49074049+mariusgtj@users.noreply.github.com>
2020-05-17 18:41:12 +0300

    Another try to solve pb with db access

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 // dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
 
 // Alternative 2
-dbPassword = 'mongodb+srv://dbUser:'+ encodeURIComponent('dbUser123') + '@cluster0-kg7ic.azure.mongodb.net/local_library_prod?retryWrites=true&w=majority';
-// dbPassword = 'mongodb+srv://dbUser:'+ encodeURIComponent(process.env.DB_PASS) + '@cluster0-kg7ic.azure.mongodb.net/local_library_prod?retryWrites=true&w=majority';
+// dbPassword = 'mongodb+srv://dbUser:'+ encodeURIComponent('dbUser123') + '@cluster0-kg7ic.azure.mongodb.net/local_library_prod?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://dbUser:'+ process.env.DB_PASS + '@cluster0-kg7ic.azure.mongodb.net/local_library_prod?retryWrites=true&w=majority';
 
 
 // Interesting is the syntax for the following module.exports:
